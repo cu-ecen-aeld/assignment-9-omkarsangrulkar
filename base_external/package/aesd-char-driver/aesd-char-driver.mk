ea275afeb7a0c40395057ed089c57c878f2d5754
omkarsangrulkar <omkarsangrulkar14@gmail.com>
2026-03-09 15:38:07 -0600

    Update package versions to de71964

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -1,4 +1,4 @@
-AESD_CHAR_DRIVER_VERSION = a47fcfa
+AESD_CHAR_DRIVER_VERSION = de71964
 AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-omkarsangrulkar.git
 AESD_CHAR_DRIVER_SITE_METHOD = git
 AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
